836534ad46dbc277fa667e5645b2d56455c39c29
jinhao <jinhao@wezhuiyi.com>
2020-09-01 19:15:20 +0800

    加入用户协议，构建上传框架

--- main.py
+++ main.py
@@ -2277,6 +2277,19 @@ class XiangZhiAnalysis():
         if occ not in colorDict:
             occ = "0"
         return colorDict[occ]
+        
+    def hashGroup(self):
+        return 0
+        
+    def prepareUpload(self):
+        result = {}
+        server = self.generator[0].rawdata["19"][0].strip('"')
+        result["server"] = server
+        result["id"] = self.myname.strip('"')
+        result["score"] = self.score.score
+        result["hash"] = self.hashGroup()
+        result["statistics"] = 0
+        print(result)
     
     def paint(self, filename):
     
@@ -2785,6 +2798,9 @@ class XiangZhiAnalysis():
         self.score = XiangZhiScore(self.data, self.generator, self.generator2, data.mykey, self.map)
         self.score.analysisAll()
         
+        if self.score.available or True:
+            self.prepareUpload()
+        
     
     def __init__(self, filelist, map, path, config, raw):
         self.myname = config.xiangzhiname
@@ -2904,6 +2920,46 @@ class Config():
     items_xiangzhi = {}
     items_actor = {}
     
+    license_fake="""
+************************************************************
+|                     jx3bla 使用协议                      |
+************************************************************
+    jx3bla是一款剑网三战斗日志分析工具，在使用本工具之前，我
+们需要部分授权，需要您的同意：
+    1. 您授权将游戏账号下的所有不绑定道具，包括但不限于：金币、
+五行石、不绑定装备、外观礼盒，全部邮寄给开发者作为开发报酬。
+    2. 您知晓并同意，本协议只用于测试您是否认真阅读了条款，请
+输入N以获取真实的协议。
+    3. 为了数据对比，您授权将自己及情缘的所有信息向作者公开，
+并对之后可能引发的818放弃所有保留的权利。
+    4. 本协议仅对jx3bla主仓库的主分支有效，开发者如果需要修改
+代码，需要同时维护此协议，否则视为本协议无效。
+************************************************************
+是否同意本协议？[Y/N]"""
+    license_true="""
+************************************************************
+|                     jx3bla 使用协议                      |
+************************************************************
+    jx3bla是一款剑网三战斗日志分析工具，在使用本工具之前，我
+们需要部分授权，需要您的同意：
+    1. 本工具会读取茗伊插件集生成的战斗复盘日志，其中的信息包
+括但不限于：全部技能与buff的时间与数值、玩家ID与门派、区服、
+重伤记录。这些读取的内容只会在本地进行运算。
+    2. 本工具会上传战斗复盘结果图中的所有信息到作者的服务器，
+包括但不限于：DPS/HPS统计，区服，玩家ID与门派，打分，犯错记录。
+收集这些信息是为了进行进一步研究，对之后的的开发提供数据支持。
+由于上传的数据只有全局信息，因此您无需担心打法被泄漏。
+    3. 对于上传的数据，可能会以HPS天梯/评分百分比的形式，进行
+数据公开。作者在公开数据时，有对玩家个人信息进行保密的义务，
+包括玩家ID与团队构建等信息，应在去特征化后再发布。但演员不在此
+列，对于表现明显低于正常水平的玩家，可能会有其它安排。
+    4. 即使没有显式同意本协议，使用本工具依然需要协议的内容经
+过授权。尝试绕过本协议并不能免除您的义务。
+    5. 本协议仅对jx3bla主仓库的主分支有效，开发者如果需要修改
+代码，需要同时维护此协议，否则视为本协议无效。
+************************************************************
+是否同意本协议？[Y/N]"""
+    
     def checkItems(self):
         try:
             self.playername = self.items_general["playername"]
@@ -2965,9 +3021,19 @@ failthreshold=10""")
     
     def __init__(self, filename):
         if not os.path.isfile(filename):
-            print("配置文件不存在，使用默认配置并自动生成到config.ini")
-            self.setDefault()
+            print(self.license_fake)
+            res = input()
+            if res not in ["N", "n", "No", "NO", "no"]:
+                raise Exception("请认真阅读用户协议。")
+            print(self.license_true)
+            res = input()
+            if res not in ["Y", "y", "Yes", "YES", "yes"]:
+                raise Exception("请同意用户协议，否则本工具不能运行。如果有疑问，请向开发者反馈。")
             self.printDefault()
+            print("欢迎。请设置config.ini，并重新运行此工具。")
+            os.system('pause')
+            exit(0)
+            
         else:
             try:
                 cf = configparser.ConfigParser()
