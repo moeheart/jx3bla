d83d76ab4a529b24cbe333f6a4839bee72f80cf1
moeheart <957685908@qq.com>
2021-10-03 12:20:01 +0800

    尝试显示数据.2

--- server.py
+++ server.py
@@ -744,6 +744,7 @@ def uploadReplayPro():
 def showReplayPro():
     id = request.args.get('id')
     db = pymysql.connect(ip, app.dbname, app.dbpwd, "jx3bla", port=3306, charset='utf8')
+    cursor = db.cursor()
     sql = """SELECT statistics FROM ReplayProStat WHERE shortID = %s OR hash = "%s"""%(id, id)
     cursor.execute(sql)
     result = cursor.fetchall()
