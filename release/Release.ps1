5ce158f807ad64fb1835389b34faeef00e6815cc
moeheart <957685908@qq.com>
2021-10-24 15:17:12 +0800

    修改发布流程

--- release/Release.ps1
+++ release/Release.ps1
@@ -3,4 +3,5 @@ git commit -m "[Auto]Update as the log"
 git push origin
 pyinstaller -F -i jx3bla.ico MainWindow.py
 mkdir dist/publish
-mv dist/MainWindow.exe dist/Main.exe
+mv dist/MainWindow.exe dist/publish/j3jz.exe
+cp -r icons dist/publish/
