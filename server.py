4ec5156b0d3256e6af5a19a97e707e8e9bb50624
moeheart <957685908@qq.com>
2022-08-04 16:51:16 +0800

    修改数据库

--- server.py
+++ server.py
@@ -573,7 +573,7 @@ def uploadActorData():
     #mapDetail = jdata["mapdetail"]
     edition = jdata["edition"]
     hash = jdata["hash"]
-    statistics = jdata["statistics"]
+    statistics = jdata["statistics"].replace('"', '`')
     
     response = {}
     
