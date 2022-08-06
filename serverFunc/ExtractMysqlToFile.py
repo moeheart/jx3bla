1e8c6e6dd335075a30199c95ba66480ffc12f36c
moeheart <957685908@qq.com>
2022-08-06 16:28:17 +0800

    尝试转移数据库

--- serverFunc/ExtractMysqlToFile.py
+++ serverFunc/ExtractMysqlToFile.py
@@ -20,17 +20,18 @@ cursor = db.cursor()
 # for start in range(START, END + 1, 1000):
 #     end = start + 1000
 #     print("Running from %d to %d" % (start, end))
-#
-#     sql = """SELECT statistics, shortID FROM ReplayProStat WHERE shortID >= %d AND shortID <= %d;"""%(start, end)
-#     cursor.execute(sql)
-#     result = cursor.fetchall()
-#
-#     for line in result:
-#         f = open("database/ReplayProStat/%d" % line[1], "w")
-#         f.write(line[0].decode())
-#         f.close()
 
-sql = """SELECT statistics, hash FROM ActorStat LIMIT 10;"""
+# sql = """SELECT statistics, shortID FROM ReplayProStat WHERE shortID >= %d AND shortID <= %d;"""%(start, end)
+sql = """SELECT statistics, shortID FROM ReplayProStat;"""
+cursor.execute(sql)
+result = cursor.fetchall()
+
+for line in result:
+    f = open("database/ReplayProStat/%d" % line[1], "w")
+    f.write(line[0].decode())
+    f.close()
+
+sql = """SELECT statistics, hash FROM ActorStat;"""
 cursor.execute(sql)
 result = cursor.fetchall()
 
